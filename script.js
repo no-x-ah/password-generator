08d1c62e855b9a5db32391a6ab54e1279caa9d8e
PnkPnther <himnishkaila.ca@gmail.com>
2025-04-30 16:59:34 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function generatePassword(length) {
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!';
+    let password = '';
+    for (let i = 0; i < length; i++) {
+      password += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    return password;
+  }
+  
+  document.getElementById('generateBtn').addEventListener('click', () => {
+    const length = parseInt(document.getElementById('lengthInput').value, 10);
+    if (!length || length <= 0) {
+      alert('Please enter a valid password length.');
+      return;
+    }
+    const pwd = generatePassword(length);
+    document.getElementById('passwordDisplay').textContent = pwd;
+  });
+  
